f39a791ff272e0258ac4bd08fc91e694cfa3b39c
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-08-27 22:45:18 +0100

    Adding the Service Provider

---
+++ src/HtmlServiceProvider.php
@@ -0,0 +1,77 @@
+<?php namespace Arcanedev\LaravelHtml;
+
+use Arcanedev\LaravelHtml\Builders\FormBuilder;
+use Arcanedev\LaravelHtml\Builders\HtmlBuilder;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class HtmlServiceProvider
+ * @package Arcanedev\LaravelHtml
+ */
+class HtmlServiceProvider extends ServiceProvider
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        $this->registerHtmlBuilder();
+        $this->registerFormBuilder();
+
+        $this->app->alias('html', HtmlBuilder::class);
+        $this->app->alias('form', FormBuilder::class);
+    }
+
+    /**
+     * Register the HTML builder instance.
+     */
+    protected function registerHtmlBuilder()
+    {
+        $this->app->bind('html', function($app) {
+            return new HtmlBuilder($app['url']);
+        });
+    }
+
+    /**
+     * Register the form builder instance.
+     */
+    protected function registerFormBuilder()
+    {
+        $this->app->bind('form', function($app) {
+            $form = new FormBuilder($app['html'], $app['url'], $app['session.store']->getToken());
+
+            return $form->setSessionStore($app['session.store']);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'html',
+            'form',
+            HtmlBuilder::class,
+            FormBuilder::class
+        ];
+    }
+
+}
