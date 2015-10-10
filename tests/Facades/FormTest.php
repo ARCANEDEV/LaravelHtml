b91c801ba0341ec5a15eb218573cbcee7d29aaf5
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-10 12:54:35 +0100

    Testing the facades

---
+++ tests/Facades/FormTest.php
@@ -0,0 +1,72 @@
+<?php namespace Arcanedev\LaravelHtml\Tests\Facades;
+
+use Arcanedev\LaravelHtml\Facades\Form;
+use Arcanedev\LaravelHtml\Tests\TestCase;
+
+/**
+ * Class     FormTest
+ *
+ * @package  Arcanedev\LaravelHtml\Tests\Facades
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class FormTest extends TestCase
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Test Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * @test
+     *
+     * @dataProvider provideOpeningForms
+     *
+     * @param  string  $expected
+     * @param  array   $options
+     */
+    public function it_can_open_form($expected, $options)
+    {
+        $this->assertEquals($expected, Form::open($options));
+    }
+
+    /** @test */
+    public function it_can_close_form()
+    {
+        $this->assertEquals('</form>', Form::close());
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Provider Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Provide opening form data
+     *
+     * @return array
+     */
+    public function provideOpeningForms()
+    {
+        $url = $this->baseUrl . '/foo';
+
+        return [
+            [
+                '<form method="GET" action="' . $url . '" accept-charset="UTF-8">',
+                ['url' => $url, 'method' => 'GET']
+            ],[
+                '<form method="POST" action="' . $url . '" accept-charset="UTF-8" class="form" id="id-form">'.
+                '<input name="_token" type="hidden">',
+                ['url' => $url, 'method' => 'POST', 'class' => 'form', 'id' => 'id-form']
+            ],[
+                '<form method="GET" action="' . $url . '" accept-charset="UTF-16">',
+                ['url' => $url, 'method' => 'GET', 'accept-charset' => 'UTF-16']
+            ],[
+                '<form method="GET" action="' . $url . '" accept-charset="UTF-16" enctype="multipart/form-data">',
+                ['url' => $url, 'method' => 'GET', 'accept-charset' => 'UTF-16', 'files' => true]
+            ],[
+                '<form method="POST" action="' . $url . '" accept-charset="UTF-8"><input name="_method" type="hidden" value="PUT">'.
+                '<input name="_token" type="hidden">',
+                ['url' => $url, 'method' => 'PUT']
+            ]
+        ];
+    }
+
+}
---
+++ tests/Facades/HtmlTest.php
@@ -0,0 +1,160 @@
+<?php namespace Arcanedev\LaravelHtml\Tests\Facades;
+
+use Arcanedev\LaravelHtml\Facades\Html;
+use Arcanedev\LaravelHtml\Tests\TestCase;
+
+/**
+ * Class     HtmlTest
+ *
+ * @package  Arcanedev\LaravelHtml\Tests\Facades
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class HtmlTest extends TestCase
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Test Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /** @test */
+    public function it_can_convert_html_string_to_entities()
+    {
+        $value  = 'Un \'apostrophe\' en <strong>gras</strong>';
+        $result = 'Un &#039;apostrophe&#039; en &lt;strong&gt;gras&lt;/strong&gt;';
+
+        $this->assertEquals($result, Html::entities($value));
+
+    }
+
+    /** @test */
+    public function it_can_convert_html_entities_to_string()
+    {
+        $value  = 'Un \'apostrophe\' en <strong>gras</strong>';
+        $result = 'Un &#039;apostrophe&#039; en &lt;strong&gt;gras&lt;/strong&gt;';
+
+        $this->assertEquals($value,  Html::decode($result));
+    }
+
+    /** @test */
+    public function it_can_make_script_tags()
+    {
+        $file = 'bootstrap.min.js';
+        $url  = $this->urlTo($file);
+
+        $this->assertEquals(
+            '<script src="' . $url . '"></script>' . PHP_EOL,
+            Html::script($file)
+        );
+    }
+
+    /** @test */
+    public function it_can_make_style_tags()
+    {
+        $file = 'bootstrap.min.css';
+        $url  = $this->urlTo($file);
+
+        $this->assertEquals(
+            '<link media="all" type="text/css" rel="stylesheet" href="' . $url . '">' . PHP_EOL,
+            Html::style($file)
+        );
+    }
+
+    /** @test */
+    public function it_can_make_image_tags()
+    {
+        $file = 'avatar.png';
+        $url   = $this->urlTo($file);
+
+        $this->assertEquals(
+            '<img src="' . $url . '">',
+            Html::image($file)
+        );
+    }
+
+    /** @test */
+    public function it_can_make_favicon()
+    {
+        $url = $this->urlTo('bar.ico');
+
+        $this->assertEquals(
+            '<link rel="shortcut icon" type="image/x-icon" href="' . $url . '">' . PHP_EOL,
+            Html::favicon($url)
+        );
+    }
+
+    /** @test */
+    public function it_can_make_link_tags()
+    {
+        $title = null;
+        $url   = $this->urlTo('hello');
+
+        $this->assertEquals(
+            '<a href="' . $url . '">' . $url . '</a>',
+            Html::link($url, $title)
+        );
+    }
+
+    /** @test */
+    public function it_can_make_secure_link_tags()
+    {
+        $title = null;
+        $url   = $this->urlTo('hello', [], true);
+
+        $this->assertEquals(
+            '<a href="' . $url . '">' . $url . '</a>',
+            Html::secureLink($url, $title)
+        );
+    }
+
+    /** @test */
+    public function it_can_make_link_tags_for_assets()
+    {
+        $file = 'style.min.css';
+        $url  = $this->baseUrl . '/style.min.css';
+
+        $this->assertEquals(
+            '<a href="' . $url . '">' . $url . '</a>',
+            Html::linkAsset($file)
+        );
+    }
+
+    /** @test */
+    public function it_can_make_dl_tags()
+    {
+        $list       = [
+            'foo'   => 'bar',
+            'bing'  => 'baz'
+        ];
+
+        $attributes = [
+            'class' => 'example'
+        ];
+
+        $this->assertEquals(
+            '<dl class="example">' .
+            '<dt>foo</dt><dd>bar</dd>' .
+            '<dt>bing</dt><dd>baz</dd>' .
+            '</dl>',
+            Html::dl($list, $attributes)
+        );
+    }
+
+    /** @test */
+    public function it_can_make_meta_tags()
+    {
+        $this->assertEquals(
+            '<meta name="description" content="Lorem ipsum dolor sit amet.">' . PHP_EOL,
+            Html::meta('description', 'Lorem ipsum dolor sit amet.')
+        );
+    }
+
+    /** @test */
+    public function it_can_make_meta_open_graph_tags()
+    {
+        $this->assertEquals(
+            '<meta content="website" property="og:type">' . PHP_EOL,
+            Html::meta(null, 'website', [
+                'property' => 'og:type'
+            ])
+        );
+    }
+}
